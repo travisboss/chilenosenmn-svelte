e705e2d1882d902256d13c2a195751c75ac48dd5
Travis Boss <travisboss@gmail.com>
2022-08-20 16:57:12 -0500

    updated announcements for party

--- src/routes/annouce.svelte
+++ src/routes/annouce.svelte
@@ -2,37 +2,44 @@
 	// import i18n
 	import { _ } from 'svelte-i18n';
 
-	const title = 'Actividades 2022';
+	const title = '¡Tiki tiki tiii amigos del Club Chileno de Minnesota!';
 	const announcements = [
 		{
 			paragraph:
-				'Después de una larga espera, ¡ha salido el sol en Minnesota! Razón suficiente para volver a vernos en persona, celebrar juntos la llegada de la primavera y volver a mostrar nuestras sonrisas. Los invitamos a dar inicio de la temporada con dos picnics:'
-		},
-		{
-			paragraph: 'Cuándo: el 25 de junio y el 13 de agosto'
-		},
-		{
-			paragraph: 'Hora: a partir de las 12 del día.'
-		},
-		{
-			paragraph: 'Lugar: Bryant Lake, 6800 Rowland Rd, Eden Prairie, MN 55344.'
+				'Como ya es tradición, nuestra fiesta de celebración de Fiestas Patrias se realizará en Snail Lake Park, el sábado 17 de septiembre. Este año tendremos stands o puestos de comida donde podrán comprar comida chilena, como empanadas, pastel de choclo y una de las novedades, berlines. '
 		},
 		{
 			paragraph:
-				'Estaremos ubicados cerca de los juegos de niños. Como ya es costumbre, el club pone el carbón para las parrillas del parque, bebidas, platos y servicios. Cada familia trae su comida, y algo extra para compartir con los demás. ¡Esto no es todo! Marquen sus calendarios para celebrar las fiestas patrias el día 17 de septiembre, en Snail Lake Pavillion at Snail Lake Regional Park, ubicado en 4191 Snail Lake Blvd, Shoreview, MN 55126. Estaremos comunicando más detalles de esta celebración más adelante.'
+				'Además, tendremos una serie de juegos y competencias durante el día, bailes folklóricos y música en vivo con los grupos musicales: “Ina Yukka” y Alma Andina.'
 		},
 		{
 			paragraph:
-				'Y el último anuncio, pero no por eso menos importante, es comunicarles que quienes integramos este comité de organización, hemos decidido dar por finalizada nuestra labor a fines del año 2022. Ha sido una larga etapa de muchos años en los que hemos puesto lo mejor de nosotros para formar una comunidad y también para crear nuevas amistades disfrutando de las tradiciones chilenas.'
+				'La entrada de preventa (hasta el miércoles 14 de septiembre) tendrá un costo de <b>$3</b> (5 a 15 años) y <b>$5</b> (mayores de 15 años). Si compras la entrada en el parque, tendrá un costo de <b>$5</b> (5 a 15 años) y <b>$10</b> (mayores de 15 años). Las entradas para nuestra fiesta podrán comprarse:'
 		},
 		{
 			paragraph:
-				'No tenemos duda de que hay personas que pueden continuar con nuestra labor y que incluso pueden hacerlo mejor. Invitamos a todos los interesados en formar un nuevo comité a comunicarse con nosotros, pues estaremos más que felices de responder sus preguntas y darles la información que necesiten.'
+				'- enviando el formulario más el pago correspondiente, en un cheque a nombre del Club Chileno de Minnesota a la dirección c/o Paula Hernández 1140 St Paul Avenue, St Paul MN 55116'
+		},
+		{
+			paragraph: ' - o a la cuenta de Venmo: @Paula-StJohn-1'
+		},
+		{
+			paragraph:
+				'No se olviden que quienes quieran participar en la venta de comida (y/u otros), deben enviar el formulario de solicitud (adjunto en este email) antes del 5 de septiembre, del 2022.'
+		},
+		{
+			paragraph:
+				'Desde ya muchas gracias a todos por su participación y esperamos verlos en nuestra celebración de Fiestas Patrias'
 		}
 	];
+
+	const image = 'https://i.postimg.cc/bY1G45r7/chileno-Image.jpg';
 </script>
 
 <section class="container mx-auto announce" id="announce">
+	<div class="flex justify-center pt-4">
+		<img class="aspect-auto" src={image} alt="party information" />
+	</div>
 	<div class="announce--wrapper">
 		<div class="announce--svg">
 			<svg
@@ -51,7 +58,7 @@
 				<h2 class="font-semibold text-blue-800 dark:text-gray-50">{title}</h2>
 				<div class="text-sm text-gray-600 dark:text-white">
 					{#each announcements as { paragraph }}
-						<p class="my-2">{paragraph}</p>
+						<p class="my-2">{@html paragraph}</p>
 					{/each}
 				</div>
 			</div>
