8e8835480e726b5ebcd73c583ee9ed59c0c4e44a
Travis <travisrboss@pm.me>
2023-02-09 06:36:11 -0600

    update carousel to TS and no swiper dep

--- src/routes/carousel.svelte
+++ src/routes/carousel.svelte
@@ -1,48 +1,55 @@
-<script>
-	import { Swiper, SwiperSlide } from 'swiper/svelte';
-	import { Pagination, Autoplay, A11y, EffectFade } from 'swiper';
+<script lang="ts">
+  import { onMount } from 'svelte';
 
-	// Import Swiper styles
-	import 'swiper/css';
-	import 'swiper/css/a11y';
-	import 'swiper/css/autoplay';
-	import 'swiper/css/pagination';
-	import 'swiper/css/effect-fade';
+  interface Slide {
+    img: string;
+    alt: string;
+  }
 
-	const slides = [
-		{
-			img: 'https://source.unsplash.com/user/loacfr/H6KJ2D0LphU/3000x600',
-			alt: 'valparaiso colorful buildings'
-		},
-		{ img: 'https://source.unsplash.com/9CPAjGVB378/3000x600', alt: 'st paul minnesota skyline' }
-	];
+  const slides: Slide[] = [
+    {
+      img: 'https://source.unsplash.com/user/loacfr/H6KJ2D0LphU/3000x600',
+      alt: 'valparaiso colorful buildings'
+    },
+    { 
+      img: 'https://source.unsplash.com/9CPAjGVB378/3000x600', 
+      alt: 'st paul minnesota skyline' 
+    }
+  ];
+
+  let currentSlideIndex = 0;
+  let intervalId: number;
+
+  onMount(() => {
+    intervalId = setInterval(() => {
+      currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    }, 5000);
+  });
 </script>
 
-<!-- <section class="container-fluid mx-auto mt-8">
-	<div class="flex justify-center">
-		<img src="https://source.unsplash.com/featured/1440x900/?chile,minneapolis" alt="" />
-	</div>
-</section> -->
+<style>
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: opacity 250ms;
+  }
 
-<section class="container-fluid mx-auto overflow-hidden hidden  sm:block">
-	<Swiper
-		modules={[Pagination, Autoplay, A11y, EffectFade]}
-		effect="fade"
-		pagination={{ clickable: true }}
-		autoplay={{
-			delay: 5000,
-			disableOnInteraction: false
-		}}
-		loop={true}
-	>
-		{#each slides as slide}
-			<SwiperSlide>
-				<img src={slide.img} alt={slide.alt} />
-			</SwiperSlide>
-		{/each}
-		<span slot="container-start" />
-		<span slot="container-end" />
-		<span slot="wrapper-start" />
-		<span slot="wrapper-end" />
-	</Swiper>
+  .slide-enter,
+  .slide-leave-to {
+    opacity: 0;
+  }
+</style>
+
+<section class="container-fluid mx-auto overflow-hidden">
+  {#each slides as slide, index}
+    <div
+      class:slide-active={index === currentSlideIndex}
+      class="fixed inset-0 h-full w-full flex items-center justify-center"
+    >
+      <img
+        class="h-64 w-full object-cover"
+        src={slide.img}
+        alt={slide.alt}
+      />
+    </div>
+  {/each}
 </section>
